6815155ee4a027d2fd449b39d4b5391517831326
rich <rich.james@dxw.com>
2022-10-17 20:35:12 +0100

    lint

--- src/components/slideshow/Slide.svelte
+++ src/components/slideshow/Slide.svelte
@@ -1,9 +1,9 @@
-
 <section class="slide-container">
   <div class="text-column">
-    <slot name="text-column"/>
+    <slot name="title" />
+    <slot name="text-column" />
   </div>
-    <slot name="image" class="slide-image"/>
+  <slot name="image" class="slide-image" />
 </section>
 
 <style>
@@ -12,33 +12,33 @@
     flex-direction: row;
   }
   .slide-container .slide-image {
-    flex:2;
+    flex: 2;
   }
   .text-column {
     flex: 1;
   }
 
-.slide-container :global(.slide-title) {
-  font-size: calc(
-    var(--base-fontsize) * var(--header-scale) * var(--header-scale) *
-      var(--header-scale) * var(--header-scale) * var(--header-scale) *
-      var(--header-scale)
-  );
-  text-align: justify;
-  width: 100%;
-  margin-bottom: 0;
-  margin-top: 0;
-}
-
-.slide-container :global(.slide-blurb) {
-  margin-top: auto;
-  grid-area: blurb;
-  height: auto;
-}
-
-@media only screen and (max-width: 1200px) {
-  .slide-container  {
-    flex-direction: column;
+  .slide-container :global(.slide-title) {
+    font-size: calc(
+      var(--base-fontsize) * var(--header-scale) * var(--header-scale) *
+        var(--header-scale) * var(--header-scale) * var(--header-scale) *
+        var(--header-scale)
+    );
+    text-align: justify;
+    width: 100%;
+    margin-bottom: 0;
+    margin-top: 0;
   }
+
+  .slide-container :global(.slide-blurb) {
+    margin-top: auto;
+    grid-area: blurb;
+    height: auto;
+  }
+
+  @media only screen and (max-width: 1200px) {
+    .slide-container {
+      flex-direction: column;
+    }
   }
 </style>
